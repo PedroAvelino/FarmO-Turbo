843d9b4e99e743cae4f8b55b502b80a08b7c6063
Pedro <pedruxavelino@gmail.com>
2020-09-23 03:36:30 -0300

    First Commit

---
+++ TodoListApp/MyCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCell.swift
+//  TodoListApp
+//
+//  Created by Rodolfo Avelino on 23/09/20.
+//
+
+import UIKit
+
+class MyCell: UITableViewCell {
+    
+
+}
---
+++ TodoListApp/Tasks_VC.swift
@@ -0,0 +1,16 @@
+//
+//  Tasks_VC.swift
+//  TodoListApp
+//
+//  Created by Pedro Avelino on 23/09/20.
+//
+
+import UIKit
+
+class Tasks_VC: ViewController{
+    
+    
+    override func viewDidLoad() {
+        title = "Tasks"
+    }
+}
