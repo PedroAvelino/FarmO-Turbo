5b1d898cd7a4781aaff4f46cabd5dcd20730ec3f
Pedro <pedruxavelino@gmail.com>
2020-10-27 16:00:16 -0300

    Added cells a implementation to create new cells

---
+++ AppData.swift
@@ -0,0 +1,8 @@
+//
+//  AppData.swift
+//  TodoListApp
+//
+//  Created by Rodolfo Avelino on 23/09/20.
+//
+
+import Foundation
---
+++ TodoListApp/hub+DataSource.swift
@@ -0,0 +1,20 @@
+//
+//  hub+DataSource.swift
+//  TodoListApp
+//
+//  Created by Rodolfo Avelino on 20/10/20.
+//
+
+import SwiftUI
+
+struct hub_DataSource: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct hub_DataSource_Previews: PreviewProvider {
+    static var previews: some View {
+        hub_DataSource()
+    }
+}
